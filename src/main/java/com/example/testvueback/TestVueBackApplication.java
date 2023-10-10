b384586d759eb0350a869b7f4529a672f13cd5d6
codky <msky0912@gmail.com>
2023-10-10 17:48:28 +0900

    Initial commit

---
+++ src/main/java/com/example/testvueback/TestVueBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.testvueback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVueBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVueBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testvueback/TestVueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testvueback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVueBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
